6235c8fb23c1c8b8be920eb57790a5c581d16bb2
Yevhen Trystan <kingguru@ukr.net>
2023-07-16 00:11:13 +0300

    Десктоп версія лендінга

--- assets/js/second.js
+++ assets/js/second.js
@@ -12,7 +12,7 @@ function open() {
 
     this.classList.add('recomm__tabs__btn-item--active');
 
-    tabContent.forEach(function (item) {
+    recContent.forEach(function (item) {
         item.classList.remove('recomm__content-item--active');
     });
     document.querySelector('#' + this.dataset.button).classList.add('recomm__content-item--active');
